e44caaa9e8f65adb4637e92678439a371f699b65
Adi <sedigoadiel@gmail.com>
2026-01-30 09:33:26 +0300

    Create 1. User Input.py

---
+++ 1. User Input.py
@@ -0,0 +1,13 @@
+
+def name_age_program():
+    print("=== Name and Age Program ===")
+    
+    
+    name = input("Please enter your name: ")
+    age = input("Please enter your age: ")
+    
+    
+    print(f"Your name is {name}, you are {age} years old")
+
+
+name_age_program()
